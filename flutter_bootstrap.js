ba6ef4ac3f670b3b8a86964c23be0e2bb73e911d
vanadizy <136460490+Vanadizy@users.noreply.github.com>
2026-06-10 14:29:47 +0300

    new look

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3746378710" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3043712456" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
